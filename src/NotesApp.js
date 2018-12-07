afcaab7a117ad1bc8505f0c3e59a1a91c4487483
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-07 14:59:25 -0500

    provides default note in case invalid id is chosen

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -7,18 +7,21 @@ class NotesApp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentNoteIndex : 0,
+            currentNoteId : 10,
             notes: [
                 {
+                    id: 1001,
                     title: 'note #1',
                     content: 'falalala'
                 },
 
                 {
+                    id:1002,
                     title: 'note #2',
                     content: 'lalalala'
                 },
                 {
+                    id: 1004,
                     title: 'note #3',
                     content: 'la'
                 }
@@ -31,19 +34,26 @@ class NotesApp extends Component {
     render() {
         return (
             <div className="notes-app">
+            <h1>NotesApp</h1>
             <SearchBar />
             <NotesList 
-             notes = {this.state.notes.map(note => note.title
-             
-                )}
+             notes = {this.state.notes.map(note => note.title)}
              />
             <EditorWindow
             content= {
-                this.state.notes[this.state.currentNoteIndex].content
+                this._getById(this.state.currentNoteId).content
             }
             />
             </div>
         );
+    }
+
+    _getById = (idToFind) =>  {
+        // find the object in this.state.notes that has the id === idToFind
+        const theOne = this.state.notes.find(note => {
+            return note.id === idToFind
+        });
+        return theOne || { content: ''};
     }
 }
 
